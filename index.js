2f6e9f349cf27dbfa93491eb01c2fe915c1483c3
ozenilsonalisson <ozenilsonalisson@gmail.com>
2023-04-13 08:51:11 -0300

    - Correções na api

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const port = process.env.PORT || 3000
 
 const fs = require('fs');
-
+const alimentos = JSON.parse(fs.readFileSync('db.json'));
 
 app.use(express.static("public"))
 app.use(express.urlencoded({extended:true}))
@@ -31,66 +31,100 @@ app.get('/', (request, response) => {
 //Listar alimentos entre M e N calorias
 app.get('/calorias', (request, response) => {
     const {menor_caloria, maior_caloria} = request.body
-
-    const alimentos = JSON.parse(fs.readFileSync('db.json'));
     
-    const alimentosFiltrados = Object.keys(alimentos)
+    const alimentosFiltrados = Object.entries(alimentos)
     .filter(alimento => {
-        const caloria = parseFloat(alimentos[alimento].Calorias);
-        return caloria >= menor_caloria && caloria <= maior_caloria;
-    }).map(alimento => alimentos[alimento]);;
+        const caloria = parseFloat(alimentos[alimento[0]].Calorias)
+        return caloria >= menor_caloria && caloria <= maior_caloria && alimento;
+    });
 
     response.status(200).json(alimentosFiltrados);
 }); 
 
 //Listar K alimentos menos carboidatros
 app.get('/menos_carbo', (request, response) => {
-    const {num} = request.body
-    dados = {"status": true}
+    const {carbo} = request.body
+
+    const alimentosFiltrados = Object.entries(alimentos)
+    .filter(alimento => {
+        return parseFloat(alimentos[alimento[0]].Carboidratos) <= carbo && alimento;
+    });
+
+    response.status(200).json(alimentosFiltrados);
     
 });
 
 //Listar K alimentos mais carboidatros
 app.get('/mais_carbo', (request, response) => {
-    const {num} = request.body
-    dados = {"status": true}
-    response.status(200).send(dados);
+    const {carbo} = request.body
+
+    const alimentosFiltrados = Object.entries(alimentos)
+    .filter(alimento => {
+        return parseFloat(alimentos[alimento[0]].Carboidratos) >= carbo && alimento;
+    });
+
+    response.status(200).json(alimentosFiltrados);
 });
 
 //Listar K alimentos menos Proteicos
 app.get('/menos_prot', (request, response) => {
-    const {num} = request.body
-    dados = {"status": true}
-    response.status(200).send(dados);
+    const {prot} = request.body
+
+    const alimentosFiltrados = Object.entries(alimentos)
+    .filter(alimento => {
+        return parseFloat(alimentos[alimento[0]].Proteinas) <= prot && alimento;
+    });
+
+    response.status(200).json(alimentosFiltrados);
 });
 
 //Listar K alimentos mais Proteicos
 app.get('/mais_prot', (request, response) => {
-    const {num} = request.body
-    dados = {"status": true}
-    response.status(200).send(dados);
+    const {prot} = request.body
+
+    const alimentosFiltrados = Object.entries(alimentos)
+    .filter(alimento => {
+        return parseFloat(alimentos[alimento[0]].Proteinas) >= prot && alimento;
+    });
+
+    response.status(200).json(alimentosFiltrados);
 });
 
 //Listar K alimentos menos Gordurosos
 app.get('/menos_gord', (request, response) => {
-    const {num} = request.body
-    dados = {"status": true}
-    response.status(200).send(dados);
+    const {gord} = request.body
+
+    const alimentosFiltrados = Object.entries(alimentos)
+    .filter(alimento => {
+        return parseFloat(alimentos[alimento[0]]['Gorduras Totais']) <= gord && alimento;
+    });
+
+    response.status(200).json(alimentosFiltrados);
 });
 
 //Listar K alimentos mais Gordurosos
 app.get('/mais_gord', (request, response) => {
-    const {num} = request.body
-    dados = {"status": true}
-    response.status(200).send(dados);
+    const {gord} = request.body
+
+    const alimentosFiltrados = Object.entries(alimentos)
+    .filter(alimento => {
+        return parseFloat(alimentos[alimento[0]]['Gorduras Totais']) >= gord && alimento;
+    });
+
+    response.status(200).json(alimentosFiltrados);
 });
 
 
 //Listar alimentos por nome
 app.get('/alimentos_nome', (request, response) => {
     const {nome} = request.body
-    dados = {"status": true}
-    response.status(200).send(dados);
+
+    const alimentosFiltrados = Object.entries(alimentos)
+    .filter(alimento => {
+        return alimento[0].includes(nome) && alimento
+    });
+
+    response.status(200).json(alimentosFiltrados);
 });
 
 
