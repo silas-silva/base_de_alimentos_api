8f61f8cf069faefbd9191b6069d4c93a2aa8fc7f
Silas Silva <silas@github.com>
2023-04-13 09:43:19 -0300

    api alimentos pasta public

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const port = process.env.PORT || 3000
 
 const fs = require('fs');
 
-//const alimentos = JSON.parse(fs.readFileSync('db.json'));
-const alimentos = require('./db');
+const alimentos = JSON.parse(fs.readFileSync('./public/db.json'));
+//const alimentos = require('./public/db');
 
 
 app.use(express.static("public"))
