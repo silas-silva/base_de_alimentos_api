bd8f2bd9946399e993f0f3ad6a0f647396bf6d58
ozenilsonalisson <ozenilsonalisson@gmail.com>
2023-04-13 09:53:58 -0300

    - Corrigindo

--- index.js
+++ index.js
@@ -25,8 +25,7 @@ app.use((request, response, next) => { //configure in cors what can access the b
 
 //rotas
 app.get('/', (request, response) => {
-    response.status(200).send("Página em desenvolvimento...")
-    alert(alimentos);
+    response.status(200).send("Página em desenvolvimento...");
 });
 
 //Listar alimentos entre M e N calorias
